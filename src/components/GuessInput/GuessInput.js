991edaf33d563efcb8916e05c2a7282f6cdef0db
Lucas Harada <lucasyharada@gmail.com>
2024-12-01 14:31:39 -0300

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { checkGuess } from "../../game-helpers";
+
+function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    checkGuess(guess, "hello");
+    console.info({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
